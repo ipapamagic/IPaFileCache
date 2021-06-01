63cd130c489c6f35a3fa9dd1af1231d81dd1cca6
ipapamagic <ipapamagic@gmail.com>
2021-06-01 16:23:36 +0800

    Initial commit

---
+++ Example/IPaFileCache/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  IPaFileCache
+//
+//  Created by ipapamagic on 06/01/2021.
+//  Copyright (c) 2021 ipapamagic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
